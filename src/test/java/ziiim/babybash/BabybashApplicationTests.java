cefa6944856c93cce71bf5d9e812360c09487c81
ZIIIIM <ZIIIIM@192.168.102.208>
2017-11-07 11:33:21 +0100

    nothing changed

---
+++ src/test/java/ziiim/babybash/BabybashApplicationTests.java
@@ -0,0 +1,16 @@
+package ziiim.babybash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BabybashApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
